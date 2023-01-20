0661f6984f940ea68e014f8c42eb581c83a4942f
Liani <lsilva9@hawk.iit.edu>
2023-01-20 15:59:35 -0600

    Initial Commit

---
+++ ErrorProblems_Ch2/ErrorProblems_Ch2App.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorProblems_Ch2App.swift
+//  ErrorProblems_Ch2
+//
+//  Created by IIT Phys 440 on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ErrorProblems_Ch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
